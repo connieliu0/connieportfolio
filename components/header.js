308025bea7c1803ccc8d8653f1be62b473498ac1
Connie Liu <connieliu@Connies-Laptop.local>
2024-12-01 23:58:40 -0500

    research

--- components/header.js
+++ components/header.js
@@ -1,7 +1,9 @@
 import styles from "./layout.module.css";
+import Head from "next/head";
 export default function Header(props) {
   return (
     <div>
+      <Head><title>{props.title}</title></Head>
       {props.header.length !== 0 && <img src={props.header} />}
       <div className={styles.header}>
         <div className={styles.headerrow}>
